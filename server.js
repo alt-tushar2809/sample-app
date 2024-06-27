c39314ffcfb3f860ec848c571881bc6f15470e28
Tushar <tushar@Tushars-MacBook-Pro.local>
2024-06-27 13:42:44 +0530

    change server.js

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Define a basic route
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello World!');
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+// Change from binding to localhost to 0.0.0.0
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is listening at http://0.0.0.0:${port}`);
 });
