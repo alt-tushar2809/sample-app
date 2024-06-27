c95d8974c4329b789fc5629dbc8a86a0f699ae1e
Tushar <tushar@Tushars-MacBook-Pro.local>
2024-06-27 15:36:38 +0530

    index.html

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+// server.js
 const express = require('express');
 const app = express();
 const port = 3000;
 
+// Serve static files from the 'public' directory (optional but useful for CSS, images, etc.)
+app.use(express.static('public'));
+
+// Serve index.html as the homepage
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.sendFile(__dirname + '/index.html');
 });
 
-// Change from binding to localhost to 0.0.0.0
+// Change from binding to localhost to 0.0.0.0 to allow external access
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is listening at http://0.0.0.0:${port}`);
+    console.log(`Server is listening at http://0.0.0.0:${port}`);
 });
